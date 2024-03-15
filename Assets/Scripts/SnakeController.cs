a8b490a01a6639e2a6cf34952ce77f89e31249b3
Chetan Rautela <chetanrautela20@gmail.com>
2024-03-15 12:00:54 +0530

    modified snake controller for movement

--- Assets/Scripts/SnakeController.cs
+++ Assets/Scripts/SnakeController.cs
@@ -1,27 +1,31 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
 
 public class SnakeController : MonoBehaviour
 {
     private Vector2Int gridposition;
     private Vector2Int gridmovedirection;
-    private float gridmovetimermax;
-    private float gridmovetimer;
-
+    //private float gridmovetimermax;
+    //private float gridmovetimer;
+    private Rigidbody2D rb;
+    [SerializeField] private int snakeSpeed = 1;
     private void Awake()
     {
         gridposition = new Vector2Int(10,10);
-        gridmovetimermax = .25f;
-        gridmovetimer = gridmovetimermax;
+       // gridmovetimermax = .25f;
+        //gridmovetimer = gridmovetimermax;
         gridmovedirection = new Vector2Int(1,0);
         RotateSprite(0f);
+        rb = GetComponent<Rigidbody2D>();
     }
 
     private void Update()
     {
         HandleInput();
         HandleGridMovement();
+        ScreenWrap();
         
     }
 
@@ -67,20 +71,62 @@ public class SnakeController : MonoBehaviour
 
     private void HandleGridMovement()
     {
-       
-        gridmovetimer += Time.deltaTime;
+
+        /*gridmovetimer += Time.deltaTime;
         if (gridmovetimer >= gridmovetimermax)
         {
             gridmovetimer -= gridmovetimermax;
             gridposition += gridmovedirection;
             
            transform.position = new Vector3(gridposition.x, gridposition.y);
-        }
-         
+        }*/
+        Vector2 movement = gridmovedirection * snakeSpeed;
+        rb.velocity = movement;
+
     }
 
     private void RotateSprite(float angle)
     {
         transform.rotation = Quaternion.Euler(0f, 0f, angle-90f);
+    }
+
+    private void ScreenWrap()
+    {
+        Vector3 currentPosition = transform.position;
+
+        //screen boundary
+        float rightBound = 20f;
+        float leftBound = 0f;
+        float upperBound = 20f;
+        float lowerBound = 0f;
+
+        // Check if the object is outside the screen bounds
+        if (currentPosition.x > rightBound)
+        {
+            currentPosition.x = leftBound;
+           
+            transform.position = currentPosition;
+        }
+        else if (currentPosition.x < leftBound)
+        {
+            currentPosition.x = rightBound;
+           
+            transform.position = currentPosition;
+        }
+
+        if (currentPosition.y > upperBound)
+        {
+            currentPosition.y = lowerBound;
+            
+            transform.position = currentPosition;
+        }
+        else if (currentPosition.y < lowerBound)
+        {
+            currentPosition.y = upperBound;
+           
+            transform.position = currentPosition;
+        }
+
+        
     }
 }
