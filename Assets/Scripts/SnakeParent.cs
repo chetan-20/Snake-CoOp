8c2f27f0f7efa73891e8c09550749437e4d91f2d
Chetan Rautela <chetanrautela20@gmail.com>
2024-03-20 20:24:20 +0530

    added snake parent

--- Assets/Scripts/SnakeParent.cs
+++ Assets/Scripts/SnakeParent.cs
@@ -4,15 +4,5 @@ using UnityEngine;
 
 public class SnakeParent : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+   protected Vector2Int gridmovedirection;
 }
